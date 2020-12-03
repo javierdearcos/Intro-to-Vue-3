6d227861ba4623d9ff1142195b8bfe00c75d5d03
Javier de Arcos <javier.dearcos@liferay.com>
2020-12-03 23:30:40 +0100

    Event listening with v-on: directive or @ shorthand

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Boots',
             description: 'Feel the power of nature with our new sustainable boots',
             image: './assets/images/socks_green.jpg',
@@ -10,9 +11,20 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeItem() {
+            this.cart -= 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 });
