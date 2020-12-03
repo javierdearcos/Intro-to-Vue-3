2bfe7b794956cb87291aaf456c9e0f24a1d8150b
Javier de Arcos <javier.dearcos@liferay.com>
2020-12-03 22:52:39 +0100

    Use v-bind to bind image source and anchor href attributes

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            description: 'Feel the power of nature with our new sustainable boots'
+            description: 'Feel the power of nature with our new sustainable boots',
+            image: './assets/images/socks_blue.jpg',
+            url: './assets/images/socks_blue.jpg'
         }
     }
 });
