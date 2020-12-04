0fc10e6e93692ed9ef0582a19aac5cf3af5e53eb
Javier de Arcos <javier.dearcos@liferay.com>
2020-12-04 22:45:20 +0100

    Use computed properties to clean code

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            brand: 'Vue Mastery',
             product: 'Boots',
             description: 'Feel the power of nature with our new sustainable boots',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://vuejs.org/',
-            inventory: 0,
+            selectedVariant: 0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            ],
+            cart: 0,
+            url: 'https://vuejs.org/'
         }
     },
     methods: {
@@ -23,11 +23,28 @@ const app = Vue.createApp({
         removeItem() {
             this.cart -= 1;
         },
-        inStock() {
-            return this.inventory > 0;
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale';
+            }
+            return '';
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        emptyCart() {
+            return this.cart == 0;
         }
     }
 });
